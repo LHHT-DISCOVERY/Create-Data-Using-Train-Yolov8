df4e89b3e42add97c5e14eb90b1425b38c1871d6
LHHT-DISCOVERY <lytri102@gmail.com>
2023-05-18 23:24:51 +0700

    Update

--- ChangeContentTxt.py
+++ ChangeContentTxt.py
@@ -1,6 +1,6 @@
 import os
 
-folder_path = "C:\Downloads\Vehicle Detection.v2-raw.yolov8\_train\labels"  # Thay thế bằng đường dẫn đến thư mục chứa các tệp tin
+folder_path = "C:\Downloads\_train2017"  # Thay thế bằng đường dẫn đến thư mục chứa các tệp tin
 
 # Lặp qua từng tệp tin trong thư mục
 for filename in os.listdir(folder_path):
@@ -12,8 +12,20 @@ for filename in os.listdir(folder_path):
             content = file.read()
 
         # Thực hiện thay thế phần nội dung
-        content = content.replace("2 ", "1 ")
+        content = content.replace(" 4 ", "4 ")
 
         # Ghi nội dung đã thay thế vào tệp tin
         with open(file_path, "w") as file:
             file.write(content)
+
+
+#  0 : bus -> 4
+#  1 : 1 car -> 0
+#  2 : máy -> 1
+#  3 : truck ko đổi
+
+# 0 : car
+# 1 : máy
+# 2 : đạp
+# 3 : truck  ko cần đổi
+# 4 : bus
